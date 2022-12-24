a374abc283ef2bb536b38ee275160be10d70e9ba
AnthonyPowell <anthonydewpow@gmail.com>
2022-12-23 19:24:44 -0600

    commit tree 32eda20169f0aeb488e7dedde5ea98c483b234dc

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = [];
+
+function writeCards(names) {
+  for (let i = 0; i < names.length; i++) {
+    cards.message = (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return cards;
+}
+
+function countDown() {
+  let countDown = 0;
+  while (countDown < 11) {
+    console.log(countDown++)
+  }
+}
